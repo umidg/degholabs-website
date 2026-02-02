6926b158e18d743009022ab5804e81bf4d3016e4
Umid Ghimire <umidghimire11@gmail.com>
2026-02-02 01:01:40 -0400

    Update logo from IMH to Degho Labs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
 
-import netlify from "@astrojs/netlify";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: netlify()
+  adapter: vercel()
 });
